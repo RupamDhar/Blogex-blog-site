9d0081097174abb3453ecef6d5a80435697595dc
RupamDhar <rupamdhar1000@gmail.com>
2025-08-03 21:20:36 +0530

    Update openai.js

--- backend/openai.js
+++ backend/openai.js
@@ -2,29 +2,23 @@ const OpenAI = require("openai");
 require("dotenv").config();
 
 exports.GenerateAISummary = async (blogContent) => {
-    try {
-        const client = new OpenAI({
-            baseURL: "https://models.github.ai/inference",
-            apiKey: process.env.GITHUB_TOKEN
-        });
-        const response = await client.chat.completions.create({
-            messages: [
-                { role: "system", content: "You are a professional blog summarizer that responds briefly and clearly in 2-3 lines." },
-                { role: "user", content: blogContent }
-            ],
-            model: "openai/gpt-4o",
-            temperature: 1,
-            max_tokens: 4096,
-            top_p: 1
-        });
-        const result = response.choices[0].message.content;
-        console.log(result);
-        return result;
-    }
-    catch (err) {
-        console.error(err);
-    }
+
+    const client = new OpenAI({
+        baseURL: "https://models.github.ai/inference",
+        apiKey: process.env.GITHUB_TOKEN
+    });
+    const response = await client.chat.completions.create({
+        messages: [
+            { role: "system", content: "You are a professional blog summarizer that responds briefly and clearly in 2-3 lines." },
+            { role: "user", content: blogContent }
+        ],
+        model: "openai/gpt-4o",
+        temperature: 1,
+        max_tokens: 4096,
+        top_p: 1
+    });
+    const result = response.choices[0].message.content;
+    console.log(result);
+    return result;
+
 }
-
-
-//github token : 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
