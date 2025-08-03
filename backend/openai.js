6bfa19f36a305f624d6dda915774595ab6fe1265
RupamDhar <rupamdhar1000@gmail.com>
2025-08-03 21:55:31 +0530

    v25.8.3.2

--- backend/openai.js
+++ backend/openai.js
@@ -2,22 +2,26 @@ const OpenAI = require("openai");
 require("dotenv").config();
 
 exports.GenerateAISummary = async (blogContent) => {
-
-    const client = new OpenAI({
-        baseURL: "https://models.github.ai/inference",
-        apiKey: process.env.GITHUB_TOKEN
-    });
-    const response = await client.chat.completions.create({
-        messages: [
-            { role: "system", content: "You are a professional blog summarizer that responds briefly and clearly in 2-3 lines." },
-            { role: "user", content: blogContent }
-        ],
-        model: "openai/gpt-4o-mini",
-        temperature: 1,
-        max_tokens: 4096,
-        top_p: 1
-    });
-    const result = response.choices[0].message.content;
-    console.log(result);
-    return result;
+    try {
+        const client = new OpenAI({
+            baseURL: "https://models.github.ai/inference",
+            apiKey: process.env.GITHUB_TOKEN
+        });
+        const response = await client.chat.completions.create({
+            messages: [
+                { role: "system", content: "You are a professional blog summarizer that responds briefly and clearly in 2-3 lines." },
+                { role: "user", content: blogContent }
+            ],
+            model: "openai/gpt-4o-mini",
+            temperature: 1,
+            max_tokens: 4096,
+            top_p: 1
+        });
+        const result = response.choices[0].message.content;
+        console.log(result);
+        return result;
+    }
+    catch (error) {
+        throw new Error(`AI Summary Generation Failed : ${error}`);
+    }
 }
